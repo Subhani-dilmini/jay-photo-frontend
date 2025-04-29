ed4ff2070d5508938a00ac6051c598a34a0828c0
Hiran Madhushanka <hiran.m@wemakeplatforms.io>
2025-04-29 22:40:32 +0530

    blog role interation

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -2,6 +2,7 @@ import { Component , OnInit} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BlogService } from '../../services/blog.service';
 import { CommonModule, NgFor } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-blog',
@@ -12,13 +13,18 @@ import { CommonModule, NgFor } from '@angular/common';
 export class BlogComponent implements OnInit{
   //global variable to store the blog data to be displayed in the template(html)
   blog: any[] = [];
+  role: any;
 
-  constructor(private blogService: BlogService) { }
+  constructor(
+    private blogService: BlogService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
     this.blogService.getBlogs().subscribe(data => {
       this.blog = data;
     })
+    this.role = this.authService.getRole();
   }
 
   getBlogs() {
